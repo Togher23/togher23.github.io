7816240d420310a9f4515f688d039be2cbfa14ea
Togher23 <togher(@gmail.com>
2017-05-15 19:06:40 +0200

    Yolo

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$(".brno").hover(
+function(){
+    $(".brno-bg").toggleClass("opacity-1");
+    $(".main-main-bg").toggleClass("opacity-0");
+}
+);
+
+$(".got").hover(
+function(){
+    $(".got-bg").toggleClass("opacity-1");
+    $(".main-main-bg").toggleClass("opacity-0");}
+);
+
+$(".mil").hover(
+function(){
+    $(".mil-bg").toggleClass("opacity-1");
+    $(".main-main-bg").toggleClass("opacity-0");}
+);
